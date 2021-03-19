fd98e8a9f0338337625a8edb00c0e92755e676f6
Hiruni <hirunihrulz@gmail.com>
2021-03-19 18:07:38 +0530

    Login and Register part
    Login and Registration UI and backend completed

--- app/src/main/java/com/example/serviceapp/Login.java
+++ app/src/main/java/com/example/serviceapp/Login.java
@@ -1,14 +1,124 @@
 package com.example.serviceapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+    EditText mEmail, mPassword;
+    Button mLoginBtn;
+    TextView mCreateBtn, forgotTextLink;
+    ProgressBar progressBar;
+//    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
+
+        mEmail = findViewById(R.id.Email);
+        mPassword = findViewById(R.id.password);
+        mLoginBtn = findViewById(R.id.loginBtn);
+        progressBar = findViewById(R.id.progressBar);
+
+        mCreateBtn = findViewById(R.id.createText);
+        forgotTextLink = findViewById(R.id.forgotPassword);
+
+        mLoginBtn.setOnClickListener(view -> {
+            String email = mEmail.getText().toString().trim();
+            String password = mPassword.getText().toString().trim();
+
+            if (TextUtils.isEmpty(email)) {
+                mEmail.setError("Email is Required.");
+                return;
+            }
+
+            if (TextUtils.isEmpty(password)) {
+                mPassword.setError("Password is Required.");
+                return;
+            }
+
+            if (password.length() < 6) {
+                mPassword.setError("Password Must be >= 6 Characters");
+                return;
+            }
+
+            progressBar.setVisibility(View.VISIBLE);
+        });
+
+        // authenticate the user
+
+//        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//            @Override
+//            public void onComplete(@NonNull Task<AuthResult> task) {
+//                if (task.isSuccessful()) {
+//                    Toast.makeText(Login.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+//                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+//                } else {
+//                    Toast.makeText(Login.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+//                    progressBar.setVisibility(View.GONE);
+//                }
+//            }
+//        });
+
+        mCreateBtn.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), Register.class)));
+
+/*
+        forgotTextLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Forget password dialog box
+                final EditText resetMail = new EditText(v.getContext());
+                final AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+                passwordResetDialog.setTitle("Reset Password ?");
+                passwordResetDialog.setMessage("Enter Your Email To Received Reset Link.");
+                passwordResetDialog.setView(resetMail);
+
+                passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    //Extract the email and send reset link
+                        String mail = resetMail.getText().toString();
+                        mAuth.sendPasswordResetEmail(mail);
+                        mAuth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(Login.this, "Reset Link Sent To Your Email.", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(Login.this, "Error ! Reset Link is Not Sent" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                    }
+                });
+
+                passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // close the dialog
+                    }
+                });
+
+                passwordResetDialog.create().show();
+
+            }
+        });
+
+*/
+
+
+            };
 }
--- app/src/main/java/com/example/serviceapp/Register.java
+++ app/src/main/java/com/example/serviceapp/Register.java
@@ -2,13 +2,89 @@ package com.example.serviceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+
 
 public class Register extends AppCompatActivity {
+    public static final String TAG = "TAG";
+    EditText mName, mEmail, mPhone, mPassword, mConfirmPass;
+    Spinner mArea;
+    Button mRegisterBtn, mClearBtn;
+    ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mName = findViewById(R.id.name);
+        mEmail = findViewById(R.id.email);
+        mPhone = findViewById(R.id.phone);
+        Spinner mySpinner = findViewById(R.id.spinner);
+        mPassword = findViewById(R.id.password);
+        mConfirmPass = findViewById(R.id.comfirmpass);
+        mRegisterBtn = findViewById(R.id.registerBtn);
+        mClearBtn = findViewById(R.id.clearBtn);
+        mProgressBar = findViewById(R.id.progressBar);
+
+        //Add drop down menu for Areas
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(Register.this,
+                android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.areas));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
+
+
+        mRegisterBtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String name = mName.getText().toString();
+                final String email = mEmail.getText().toString().trim();
+                final String phone  = mPhone.getText().toString();
+                String comfirmpass = mConfirmPass.getText().toString();
+                String password = mPassword.getText().toString().trim();
+                //setup overide method for spinner
+
+                if (TextUtils.isEmpty(email)) {
+                    mEmail.setError("Email is Required.");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    mPassword.setError("Password is Required.");
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    mPassword.setError("Password Must be >= 6 Characters");
+                    return;
+                }
+
+                mProgressBar.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+        mRegisterBtn.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(),Lobby.class)));
+
+        mClearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mName.setText("");
+                mEmail.setText("");
+                mPhone.setText("");
+                mPassword.setText("");
+                mConfirmPass.setText("");
+            }
+        });
+
     }
 }
